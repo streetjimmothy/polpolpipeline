44f959f15a2c5c57a7a51281783ebff22bf293cb
James Bonner <jabcmes@outlook.com>
2024-01-21 19:51:51 +1100

    githib actions file structure familiarisiation

--- deploy.py
+++ deploy.py
@@ -1,4 +1,12 @@
 import os
 
-for name, value in os.environ.items():
-	print(f"{name}: {value}")
+parent_dir = os.path.join(os.getcwd(), '..')
+
+for filename in os.listdir(parent_dir):
+    print(filename)
+
+
+parent_dir = os.environ.get('GITHUB_WORKSPACE')
+
+for filename in os.listdir(parent_dir):
+    print(filename)
