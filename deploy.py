e70f76afa5c3e1a8ff69b93de455185d414e9bca
James Bonner <jabcmes@outlook.com>
2024-01-23 13:21:49 +1100

    removed test code

--- deploy.py
+++ deploy.py
@@ -1,12 +1,2 @@
 import os
 
-parent_dir = os.path.join(os.getcwd(), '..')
-
-for filename in os.listdir(parent_dir):
-    print(filename)
-
-
-parent_dir = os.environ.get('GITHUB_WORKSPACE')
-
-for filename in os.listdir(parent_dir):
-    print(filename)
