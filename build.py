a5a675c5a6467e9e1ee7e2efba2c3c66459a2b1a
James Bonner <jabcmes@outlook.com>
2024-02-16 13:15:54 +1100

    init build and pipeline

--- build.py
+++ build.py
@@ -1,2 +1,33 @@
-import os
+import sys
+import logging
+logFormatter = logging.Formatter("%(asctime)s::%(levelname)s:%(message)s")
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.DEBUG)
 
+fileHandler = logging.FileHandler("build.log")
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+import subprocess
+
+#instead of doing this, go through each folder and get the requirements file
+subprocess.check_call([sys.executable, "-m", "pip", "install", "humanize"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "pymongo"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "networkx"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "igraph"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "leidenalg"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "sklearn"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "psutil"])
+
+#might be necessary to install pycairo
+# sudo apt-get update -y
+# sudo apt-get install -y pkg-config
+# sudo apt-get install libcairo2-dev libjpeg-dev libgif-dev
+# sudo pip install pycairo
+subprocess.check_call([sys.executable, "-m", "pip", "install", "pycairo"])
--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,25 @@
-#download data
-#import script from 
+import sys
+import logging
+logFormatter = logging.Formatter("%(asctime)s::%(levelname)s:%(message)s")
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.DEBUG)
+
+fileHandler = logging.FileHandler("pipeline.log")
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+from db import DB
+
+DB = DB()
+DB.run_query()
+DB.post_process()
+
+#iterate over all folders in the target directory
+# need to make them packages in build.py
+# path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../polpolpipeline/"))
+# 	sys.path.append(path)
+# 	import db
